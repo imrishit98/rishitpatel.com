6b63a4b7bac8610092c7a07c4c5215eae9b94cb8
imrishit98 <imrishit98@gmail.com>
2020-06-29 22:48:29 -0400

    Added chinese alternative apps

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/rp-logo.svg`, // This path is relative to the root of the site.
+        icon: `src/images/rp-logo.svg`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

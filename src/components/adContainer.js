cb58ca49f11ae66315595e2feb77453628037a77
imrishit98 <imrishit98@gmail.com>
2020-09-08 19:59:36 -0400

    adsense fix

--- src/components/adContainer.js
+++ src/components/adContainer.js
@@ -4,11 +4,7 @@ import AdSense from "react-adsense"
 const AdContainer = props => {
   return (
     <>
-      <AdSense.Google
-        client="ca-pub-1201474411749621"
-        slot="9205903697"
-        responsive="true"
-      />
+      <AdSense.Google client="ca-pub-1201474411749621" slot="9205903697" />
     </>
   )
 }

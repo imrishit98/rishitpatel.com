e4c7ccd327b37d58c4e0b35afec3d92b5af03370
Rishit <imrishit98@gmail.com>
2022-10-16 23:25:32 -0400

    Add mvp Hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,37 @@ import { Container } from '../components/ui/containers';
 const Home = () => {
   return (
     <Layout>
-      <Container className='flex justify-center items-center text-center h-[85vh]'>
-        <h1 className='text-4xl font-bold'>
-          Hi, I&apos;m Rishit Patel. <br /> I&apos;m a Full Stack Developer.
-        </h1>
+      <Container className='flex flex-col justify-center items-center text-center h-[85vh] 3xl:h-[50vh]'>
+        <h1 className='text-5xl md:text-6xl'>
+          Hi! I&apos;m <br className='md:hidden' />
+          <strong>Rishit Patel</strong>.
+        </h1>{' '}
+        <h2 className='text-2xl sm:text-3xl'>
+          <span
+            className='animate-fade-in-left'
+            style={{ animationDelay: '200ms' }}
+          >
+            A
+          </span>{' '}
+          <span
+            className='animate-fade-in-left'
+            style={{ animationDelay: '500ms' }}
+          >
+            Full
+          </span>{' '}
+          <span
+            className='animate-fade-in-left'
+            style={{ animationDelay: '800ms' }}
+          >
+            Stack
+          </span>{' '}
+          <span
+            className='animate-fade-in-left'
+            style={{ animationDelay: '1100ms' }}
+          >
+            Developer.
+          </span>
+        </h2>
       </Container>
     </Layout>
   );

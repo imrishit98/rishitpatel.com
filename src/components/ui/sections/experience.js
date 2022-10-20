39552401236dfd6fbf7cb018b2be1fc021f8cf06
Rishit <imrishit98@gmail.com>
2022-10-19 22:24:35 -0400

    Change my journey component to experience

--- src/components/ui/sections/experience.js
+++ src/components/ui/sections/experience.js
@@ -1,44 +1,44 @@
-import Section from './index';
-import { JourneyCard } from '../cards';
+import { ExperienceCard } from '../cards';
 import { Container } from '../containers';
-import { H2, H3, H4, H5 } from '../typography';
+import { H2, H3, H5 } from '../typography';
+import Section from './index';
 
-const MyJourney = () => {
+const Experience = () => {
   return (
-    <Section>
+    <Section id='experience'>
       <Container className='flex flex-col justify-center items-center gap-10 text-center'>
-        <H2>My Journey</H2>
+        <H2>Experience</H2>
         <div className='grid gap-6 md:gap-8'>
-          <JourneyCard imgSrc='/images/logos/ion8.jpg' imgAlt='ion8 logo'>
+          <ExperienceCard imgSrc='/images/logos/ion8.jpg' imgAlt='ion8 logo'>
             <H3>Software Designer</H3>
             <H5>ion8.net</H5>
             <H5>May 2020 - Present</H5>
-          </JourneyCard>
-          <JourneyCard
+          </ExperienceCard>
+          <ExperienceCard
             imgSrc='/images/logos/web3-academy-dao.jpg'
             imgAlt='ion8 logo'
           >
             <H3>Core Team Member</H3>
             <H5>Web3 Academy DAO</H5>
             <H5>July 2022 - Present</H5>
-          </JourneyCard>
-          <JourneyCard imgSrc='/images/logos/zedd.jpg' imgAlt='ion8 logo'>
+          </ExperienceCard>
+          <ExperienceCard imgSrc='/images/logos/zedd.jpg' imgAlt='ion8 logo'>
             <H3>Sales Representative</H3>
             <H5>Zedd Customer Solutions</H5>
             <H5>September 2018 - July 2020</H5>
-          </JourneyCard>
-          <JourneyCard
+          </ExperienceCard>
+          <ExperienceCard
             imgSrc='/images/logos/torofi-broadband.png'
             imgAlt='ion8 logo'
           >
             <H3>Network Engineer</H3>
             <H5>Torofi Networks Private Limited</H5>
             <H5>March 2016 - June 2017</H5>
-          </JourneyCard>
+          </ExperienceCard>
         </div>
       </Container>
     </Section>
   );
 };
 
-export default MyJourney;
+export default Experience;

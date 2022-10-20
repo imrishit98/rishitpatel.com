c82fd82a80ed51c4986f758270dfd6a7ee5a9c78
Rishit <imrishit98@gmail.com>
2022-10-19 23:25:01 -0400

    Update navbar links

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <nav className='py-4 shadow-sm bg-blue-100 dark:bg-dark'>
-      <Container className='flex justify-between items-center'>
+    <nav className='fixed w-full z-50 py-2 shadow-sm bg-blue-100 dark:bg-dark'>
+      <Container className='flex flex-col justify-between items-center'>
         <div className='flex items-center '>
           <Link href='/'>
             <a className='text-2xl font-bold text-gray-800 hover:text-gray-700 dark:text-white dark:hover:text-gray-300 lg:text-3xl'>
@@ -16,19 +16,19 @@ const Navbar = () => {
           </Link>
         </div>
         <div className='flex'>
-          <Link href='/'>
-            <a className='px-4 py-2 text-sm font-medium hover:font-bold'>
-              Home
-            </a>
-          </Link>
-          <Link href='/blog'>
-            <a className='px-4 py-2 text-sm font-medium hover:font-bold'>
-              Blog
-            </a>
-          </Link>
-          <Link href='/about'>
+          <Link href='/#about'>
             <a className='px-4 py-2 text-sm font-medium hover:font-bold'>
               About
+            </a>
+          </Link>
+          <Link href='/#experience'>
+            <a className='px-4 py-2 text-sm font-medium hover:font-bold'>
+              Experience
+            </a>
+          </Link>
+          <Link href='/#projects'>
+            <a className='px-4 py-2 text-sm font-medium hover:font-bold'>
+              Projects
             </a>
           </Link>
           <button

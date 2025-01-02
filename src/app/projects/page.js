ef6e18295d757953ef44939329d8b1f20db28244
Rishit <imrishit98@gmail.com>
2025-01-02 01:26:25 -0500

    Add ProjectsPage component and related sub-components

---
+++ src/app/projects/page.js
@@ -0,0 +1,29 @@
+import { ProjectsGrid } from '@/components/ui/projects/projects-grid';
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from '@/components/ui/page-header';
+
+export const metadata = {
+  title: 'Projects - Rishit Patel',
+  description:
+    'Explore my portfolio of projects, including directories, SaaS applications, and more.',
+};
+
+export default function ProjectsPage() {
+  return (
+    <div className='container relative pb-8'>
+      <PageHeader className='pb-8'>
+        <PageHeaderHeading>Projects</PageHeaderHeading>
+        <PageHeaderDescription>
+          A collection of my work, ranging from directory websites to SaaS applications.
+          Each project is built with modern technologies and designed with user
+          experience in mind.
+        </PageHeaderDescription>
+      </PageHeader>
+
+      <ProjectsGrid />
+    </div>
+  );
+}

4937cb0bbe8ce2d2092c29aa76911fd8dbb907d7
Rishit Patel <imrishit98@gmail.com>
2026-01-12 23:31:58 -0500

    Refactor Container component: simplify className concatenation for cleaner code.

--- src/components/ui/containers.js
+++ src/components/ui/containers.js
@@ -11,12 +11,7 @@ import { cn } from '@/utils/shadcn';
  */
 export const Container = ({ className, children }) => {
   return (
-    <div
-      className={cn(
-        'container w-full px-2 mx-auto md:px-8 lg:px-20 xl:px-40',
-        className
-      )}
-    >
+    <div className={cn('container w-full px-2 mx-auto md:px-8 lg:px-20', className)}>
       {children}
     </div>
   );
